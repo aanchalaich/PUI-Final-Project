0b132c6eceb9f7a8d2eba916d056fff29a1c4477
aanchalaich <aanchalaich@gmail.com>
2020-12-06 23:31:42 -0500

    scroll works now

--- newStuff/growth/interactive.js
+++ newStuff/growth/interactive.js
@@ -8,6 +8,210 @@ function changeImage(id, imgSource) {
 
 $(".animated").addClass("slower");
 
+function corpGov(){
+
+  d3.select("SVG").remove()
+
+  const width = 500;
+const height = 500;
+const margin = 0;
+
+const adj = 30;
+var paddingLeft = 50, paddingRight = 40, paddingTop = 10, paddingBottom = 10;
+// we are appending SVG first
+const svg = d3.select("div#container").append("svg")
+  .attr("preserveAspectRatio", "xMinYMin meet")
+  .attr("viewBox", "-"
+    + adj + " -"
+    + adj + " "
+    + (width + adj * 3) + " "
+    + (height + adj * 3))
+
+  .classed("svg-content", true);
+
+                //-----------------------------DATA------------------------------//
+
+
+
+
+
+        var nameObj = {
+                 "Year": [2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019],
+                 "Rev": [8490, 10711, 14835, 19166, 24509, 34204, 48077, 61093, 74452, 88988, 107006, 135987, 177866, 232887, 280522],
+                 "Salary": [81840, 81840, 81840, 81840, 81840, 81840, 81840, 81840, 81840, 81840, 81840, 81840, 81840, 81840, 81840]
+             };
+
+             const dataset = nameObj;
+
+             const years = [2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019];
+             const rev = [8490, 10711, 14835, 19166, 24509, 34204, 48077, 61093, 74452, 88988, 107006, 135987, 177866, 232887, 280522];
+             const salary = [81840, 81840, 81840, 81840, 81840, 81840, 81840, 81840, 81840, 81840, 81840, 81840, 81840, 81840, 81840];
+
+             finalArray = {
+                 x: [2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019],
+                 y: [8490, 10711, 14835, 19166, 24509, 34204, 48077, 61093, 74452, 88988, 107006, 135987, 177866, 232887, 280522]
+             };
+
+             finalArray2 = {
+                 x: [2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019],
+                 y: [81840, 81840, 81840, 81840, 81840, 81840, 81840, 81840, 81840, 81840, 81840, 81840, 81840, 81840, 81840]
+             };
+
+             var rearrangedData = finalArray.x.map(function (d, i) {
+                 return { x: d, y: finalArray.y[i] };
+
+             })
+
+             var rearrangedData2 = finalArray2.x.map(function (d, i) {
+                 return { x: d, y: finalArray.y[i] };
+
+             })
+
+
+
+             var xscale = d3.scaleTime()
+                 .domain([d3.min(finalArray.x), d3.max(finalArray.x)])
+                 .range([paddingLeft, width - paddingRight]);
+             svg.append("g")
+                 .attr("transform", "translate(0," + width / 2 + ")")
+                 .call(d3.axisBottom(xscale).tickFormat(d3.format("d")));
+
+
+             var yscale = d3.scaleLinear()
+                 .domain([d3.min(finalArray.y), 300000])
+                 .range([height / 2 - paddingBottom, paddingTop]);
+             svg.append("g")
+                 .attr("transform", "translate(50, 10)")
+                 .call(d3.axisLeft(yscale));
+
+
+
+             var yscale2 = d3.scaleLinear()
+                 .domain([d3.min(finalArray2.y), d3.max(finalArray2.y)])
+                 .range([height / 2 - paddingBottom, paddingTop]);
+             svg.append("g")
+                 .attr("transform", "translate(50, 10)")
+                 .call(d3.axisLeft(yscale));
+
+
+
+             var valueline = d3.line()
+                 .x(function (d) { return xscale(d.x); })
+                 .y(function (d) { return yscale(d.y); });
+
+             var valueline2 = d3.line()
+                 .x(function (d) { return xscale(d.x); })
+                 .y(function (d) { return yscale2(d.y); });
+
+
+
+
+             svg.append("path")
+                 .attr("fill", "none")
+                 .attr("stroke", "steelblue")
+                 .attr("stroke-width", 1.5)
+                 .attr("d", valueline(rearrangedData));
+
+             svg.append("path")
+                 .attr("fill", "none")
+                 .attr("stroke", "red")
+                 .attr("stroke-width", 1.5)
+                 .attr("d", valueline2(rearrangedData2));
+}
+
+function segment(){
+
+  d3.select("SVG").remove()  
+  
+  const treeData = {
+    "name": "Amazon",
+    "value": 15,
+    "type": "black",
+    "level": "yellow",
+    "children": [
+        {
+            "name": "North America",
+            "value": 10,
+            "type": "grey",
+            "level": "red"
+        },
+        {
+            "name": "International",
+            "value": 10,
+            "type": "grey",
+            "level": "red",
+            
+        },
+        {
+            "name": "Amazon Web Services",
+            "value": 10,
+            "type": "grey",
+            "level": "blue"
+        },
+        
+    ]
+  };
+
+// set the dimensions and margins of the diagram
+const margin = { top: 20, right: 200, bottom: 30, left: 200 },
+    width = 660 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+
+// declares a tree layout and assigns the size
+const treemap = d3.tree().size([height, width]);
+
+//  assigns the data to a hierarchy using parent-child relationships
+let nodes = d3.hierarchy(treeData, d => d.children);
+
+// maps the node data to the tree layout
+nodes = treemap(nodes);
+
+// append the svg object to the body of the page
+// appends a 'group' element to 'svg'
+// moves the 'group' element to the top left margin
+const svg = d3.select("body").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom),
+    g = svg.append("g")
+        .attr("transform",
+            "translate(" + margin.left + "," + margin.top + ")");
+
+// adds the links between the nodes
+const link = g.selectAll(".link")
+    .data(nodes.descendants().slice(1))
+    .enter().append("path")
+    .attr("class", "link")
+    .style("stroke", d => d.data.level)
+    .attr("d", d => {
+        return "M" + d.y + "," + d.x
+            + "C" + (d.y + d.parent.y) / 2 + "," + d.x
+            + " " + (d.y + d.parent.y) / 2 + "," + d.parent.x
+            + " " + d.parent.y + "," + d.parent.x;
+    });
+
+// adds each node as a group
+const node = g.selectAll(".node")
+    .data(nodes.descendants())
+    .enter().append("g")
+    .attr("class", d => "node" + (d.children ? " node--internal" : " node--leaf"))
+    .attr("transform", d => "translate(" + d.y + "," + d.x + ")");
+
+// adds the circle to the node
+node.append("circle")
+    .attr("r", d => d.data.value)
+    .style("stroke", d => d.data.type)
+    .style("fill", d => d.data.level);
+
+// adds the text to the node
+node.append("text")
+    .attr("dy", ".35em")
+    .attr("x", d => d.children ? (d.data.value + 5) * -1 : d.data.value + 5)
+    .attr("y", d => d.children && d.depth !== 0 ? -(d.data.value + 5) : d)
+    .style("text-anchor", d => d.children ? "end" : "start")
+    .text(d => d.data.name);
+}
+
+
 
 /*$(window).scroll(function makeCompensationGraph(){ 
   //------------------------1. PREPARATION-------------------------//
